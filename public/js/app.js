ae4bd9b33a484c445921c7d2a2ad4517adc6e981
Xavi <>
2018-10-02 19:34:24 +0200

    Working solution

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,12 @@ window.onload = function () {
   var marker;
   var selectedPlace;
 
+  const facilities = {
+    wc: '5b921960e9fb4e0022eb95e3',
+    restaurant: '5b917690db22200013722018',
+    disabled: '5b921960e9fb4e0022eb95e3'
+  }
+
   const initConfig = {
     bearing: 48.1,
     zoom: 19,
@@ -25,6 +31,7 @@ window.onload = function () {
     '57285b8a7f0f7a0b0092cf5e'
   ]
   var departments = {};
+  var allPlaces;
 
    Mapwize.apiKey('3d2dafbf53a14c95cee47c2348f9c5c3'); // This is a demo key, giving you access to the demo building. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signin up at mapwize.io
    mapwizeMap = new Mapwize.Map({
@@ -59,10 +66,14 @@ window.onload = function () {
        loadPlaces();
      });
 
+     mapwizeMap.on('mapwize:floorchange', floor => {
+       app.selectedFloor = floor.floor;
+     });
 
      mapwizeMap.on('mapwize:click', e => {
        if (e.place !== null){
          showPlace(e.place);
+         app.navigateDisabled = false;
        }
           // app.name = e.place.name;
           // app.articles = e.place.data ? e.place.data.articles : [];
@@ -97,6 +108,7 @@ window.onload = function () {
     }
 
     var navigate = () => {
+      if (selectedPlace === undefined){return false;}
        Mapwize.Api.getDirection({
         from: {
           latitude: 47.42209953906886,
@@ -120,6 +132,7 @@ window.onload = function () {
         var floors = mapwizeMap.getFloors();
         floors.sort();
         console.log(places);
+        allPlaces = Object.assign({}, places);
         departments = floors.map((currentValue)=>(departments[currentValue] = []));
         places.forEach((element)=>{
           const floor = element.floor;
@@ -129,9 +142,18 @@ window.onload = function () {
           }
         })
         console.log(departments);
+        console.log(allPlaces);
         app.departments = departments;
       });
     }
+
+    var findPlaceById = (placeId) => {
+      if (allPlaces === undefined) {return false;}
+      const i = Object.values(allPlaces);
+      return i.find((element)=>{
+        return element._id === placeId ? true : false;
+      })
+    };
 
 
 
@@ -141,10 +163,11 @@ window.onload = function () {
           name: '',
           articles: art,
           departments: departments,
-          navigation: false,
+          navDisplay: false,
           distance: '',
           time: '',
-          'selectedFloor' : initConfig.floor,
+          selectedFloor : initConfig.floor,
+          navigateDisabled: false,
           map: mapwizeMap
         },
         computed: {
@@ -155,17 +178,28 @@ window.onload = function () {
         methods: {
           clickOnPlace: function (place){
             showPlace(place);
+            this.navigateDisabled = false;
           },
-          clickOnFloor: (floor) => {
-            console.log(floor);
+          clickOnFloor: function (floor)  {
             changeFloor(floor);
           },
-          clickOnNavigate: (event) =>{
+          clickOnNavigate: function (event) {
             navigate();
+            this.navDisplay= true;
           },
-          clickOnCancelNavigate: (event) => {
+          clickOnCancelNavigate: function (event)  {
             mapwizeMap.removeDirection();
             initPosition();
+            this.navigateDisabled = true;
+            this.navDisplay = false;
+
+          },
+          clickOnGoTo: function(placeName)  {
+            const _place = findPlaceById(facilities[placeName]);
+            console.log(_place);
+            showPlace(_place);
+            navigate();
+            this.navDisplay = true;
           }
         }
       }); //VUE
